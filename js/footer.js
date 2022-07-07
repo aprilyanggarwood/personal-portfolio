331cb59628cdf647c44ecdf795d05be741a2f4bd
aprilyanggarwood <yangyyj822@hotmail.com>
2022-07-06 23:26:45 -0700

    worked with header and footer

---
+++ js/footer.js
@@ -0,0 +1,58 @@
+/* 
+Title: footer.js
+Author: April Yang
+Date: 07/06/2022
+Description: Web component for footer
+*/
+
+class FooterComponent extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+ <footer>
+
+    <div class="grid">
+    <div class="row">
+    <div class="col-4">
+    <p class="copyRight">&copy; Copyright April Yang</p>
+    </div>
+    <div class="col-8">
+
+<div class="listFooter">
+  <ul class="nav">
+  <li class="nav-item">
+    <a class="navLink footerLink" href=""><i class="fa-brands fa-linkedin"></i></a>
+  </li>
+  <li class="nav-item">
+    <a class="navLink footerLink" href="https://github.com/aprilyanggarwood/personal-portfolio" target="_blank"><i class="fa-brands fa-github"></i></a>
+  </li>
+  <li class="nav-item">
+    <a class="navLink footerLink" href="Web Development Degree" target="_blank"><i class="fa-solid fa-file-lines"></i></a>
+  </li>
+
+  <li class="nav-item">
+  <span class="vl footerLink"></span>
+</li>
+<li class="nav-item">
+  <p class="footerBU">BU</p>
+</li>
+   <li class="nav-item">
+    <a class="navLink footerLink" href="https://github.com/buwebdev" target="_blank"><i class="fa-brands fa-github"></i></a>
+  </li>
+  <li class="nav-item">
+    <a class="navLink footerLink" href='https://www.youtube.com/c/bellevueuniversity' target="_blank"><i class="fa-brands fa-youtube"></i></a>
+  </li>
+</ul>
+</div>
+</div>
+
+</footer>
+  
+        `;
+  }
+}
+
+customElements.define("footer-component", FooterComponent);
---
+++ js/header.js
@@ -0,0 +1,54 @@
+/* 
+Title: header.js
+Author: April Yang
+Date: 07/06/2022
+Description: Web component for Headers
+*/
+
+class HeaderComponent extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+<nav>
+<div class="navbar">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0">April Yang Personal Portfolio</span>
+  </div>
+</div>
+</nav>
+<hr>
+
+<nav class="nav-div">
+<div class="listNavbar">
+  <ul class="nav">
+  <li class="nav-item">
+    <a class="navLink" href="./index.html">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="navLink" href="./projects.html">Projects</a>
+  </li>
+  <li class="nav-item">
+    <a class="navLink" href="./database.html">Database Diagrams</a>
+  </li>
+  <li class="nav-item">
+    <a class="navLink" href="./api.html">API Unit Tests</a>
+  </li>
+  <li class="nav-item">
+    <a class="navLink" href="./resume.html">Resume</a>
+  </li>
+  <li class="nav-item">
+    <a class="navLink" href="./about.html">About</a>
+  </li>
+</ul>
+</div>
+</nav>
+
+
+      `;
+  }
+}
+
+customElements.define("header-component", HeaderComponent);
